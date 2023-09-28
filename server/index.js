f4fbb9e418c99a1a15671f14b6756cf61a258d29
Jhonatan <developerengine12@gmail.com>
2023-09-28 18:55:55 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const { text } = require('express')
+
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {cors: {origin: 'http://localhost:5173'}})
+
+const PORT = 2724
+
+io.on("connection", socket =>{
+    console.log("usuario conectado", socket.id);
+
+    socket.on("disconnect", reason => {
+        console.log("Usuario desconectado", socket.id)
+    })
+
+    socket.on("set_username", username => {
+        socket.data.username = username
+        console.log(socket.data.username)
+    })
+
+    socket.on("message", text=>{
+        io.emit("receive_message", {
+            text,
+            authorId: socket.id,
+            author: socket.data.username
+        })
+    })
+})
+
+server.listen(PORT, () => console.log('Server runnig...'))
